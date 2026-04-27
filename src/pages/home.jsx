48affbd2ec4eebee4df314e7db9d9318175c4920
James Junior Baah <jjbaah068@gmail.com>
2026-04-27 21:22:39 +0000

    updates to components naming

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,7 +2,7 @@ import Navbar from "./../components/navbar";
 import Hero from "./../components/hero";
 import herobg from "../assets/videos/hero.mp4"; 
 import Amenities from "./../components/amenities";  
-import FeaturedApartments from "./../components/featuredApartments";
+import FeaturedApartments from "./../components/featuredapartments";
 import Testimonials from "./../components/testimonials";
 import Footer from "./../components/footer";
 
